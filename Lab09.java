ded559d32fb67c26114f24a43373ff0216219642
Kent Collins <kent.r.collins@gmail.com>
2014-11-13 22:15:55 +0000

    updated

--- Lab09.java
+++ Lab09.java
@@ -1,11 +1,33 @@
+/**
+ * A robot moves along a row, collecting piles of beepers and placing them one square to
+ * the right.
+ * 
+ * @author kentcollins
+ *
+ */
 import edu.fcps.karel2.Display; 
+import javax.swing.JOptionPane;
 
 public class Lab09 {
 	 
 	 public static void main(String[] args) {
-		 Display.openWorld("maps/piles1.map");
+		 String map = JOptionPane.showInputDialog("Which map?");
+		 Display.openWorld("maps/"+map+".map");
 		 Display.setSize(10, 10);
 		 Athlete athena = new Athlete(1, 1, Display.EAST, 0);
-		 
+		 for (int count = 1; count<=8; count++) {
+			 int beepersHeld = athena.getBeepers();
+			 while (athena.nextToABeeper()) {
+				 athena.pickBeeper();
+			 }
+			 for (int i = 0; i<beepersHeld; i++) {
+				 athena.putBeeper();
+			 }
+			 athena.move();
+		 }
+		 athena.turnAround();
+		 while(athena.frontIsClear()) {
+			 athena.move();
+		 }
 	 }
  }
