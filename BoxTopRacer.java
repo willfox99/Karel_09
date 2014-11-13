ff78a9f0f65c8b591a83884199f20407ae5f8562
Kent Collins <kent_collins@asl.org>
2014-11-13 08:47:41 +0000

    included instructions for adding a constructor

--- BoxTopRacer.java
+++ BoxTopRacer.java
@@ -7,6 +7,9 @@
  */
 public class BoxTopRacer extends Racer {
 	
+	//TODO Subclasses do not inherit any constructors from their superclass
+	//Consequently, you will need to provide a compatible constructor
+	
 	/**
 	 * Re-implement the method to allow for jumping hurdles of any width
 	 */
