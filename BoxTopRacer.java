6ceaf72f6d8f924c66bd92155b82b81907f473b9
Kent Collins <kent_collins@asl.org>
2014-11-13 08:28:50 +0000

    added skeleton classes

---
+++ BoxTopRacer.java
@@ -0,0 +1,18 @@
+/**
+ * A BoxTopRacer is a Racer that can jump over hurdles of any width.
+ * 
+ * @author <...>
+ * @version <...>
+ *
+ */
+public class BoxTopRacer extends Racer {
+	
+	/**
+	 * Re-implement the method to allow for jumping hurdles of any width
+	 */
+	@Override
+	public void jumpRight() {
+		
+	}
+
+}
---
+++ SteepleChaseRacer.java
@@ -0,0 +1,18 @@
+/**
+ * A SteepleChaseRacer is a Racer that can jump over hurdles of any height.
+ * 
+ * @author <...>
+ * @version <...>
+ *
+ */
+public class SteepleChaseRacer extends Racer {
+	
+	/**
+	 * Re-implement the method to allow for jumping hurdles of any height
+	 */
+	@Override
+	public void jumpRight() {
+		
+	}
+
+}
